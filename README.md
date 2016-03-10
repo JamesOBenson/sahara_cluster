d312ba035ca0ab198f8a2c5c357583669076dda4
James Benson <James.o.benson@gmail.com>
2016-03-10 15:54:14 -0600

    Initial Post

---
+++ README.md
@@ -0,0 +1,39 @@
+# Sahara HortonWorks Cluster Deploy
+---
+This script is designed to work with an Openstack cluster that has Sahara deployed on it. 
+
+**Requirements:**
+Openstack
+Sahara
+Access to controller node
+
+##How to setup cluster
+1. Open *create_hadoop_cluster.sh* 
+2. Modify list of variables contained inside, particularly:
+  *  FLOATING_IP_POOL - Should be your external network
+  *  NETWORK_NAME - Should be your internal network
+  *  IMAGE_NAME - Your base image for Hortonworks, this can be aquired from the sahara image repository: 
+     http://sahara-files.mirantis.com/images/upstream/liberty/
+     or built from the diskimage-builder:
+     https://github.com/openstack/diskimage-builder
+3. Once complete, source your openrc
+4. Run sh create_hadoop_cluster.sh
+
+##What it will do
+1. Copy unmodified json files to a new directory
+2. Update json files with new values
+3. Create the worker & master node template
+4. Create the cluster group template
+5. Create a new ssh key
+6. Update the my_cluster_create.json file which is the cluster you will deploy
+7. Deploy the new cluster.
+
+##How is this playbook licensed?
+
+It's licensed under the Apache License 2.0. The quick summary is:
+
+> A license that allows you much freedom with the software, including an explicit right to a patent. “State changes” means that you have to include a notice in each file you modified. 
+
+Pull requests and Github issues are welcom!
+
+-- James
---
+++ create_hadoop_cluster.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+CLUSTER_NAME="MyCluster"
+PLUGIN_NAME="ambari"
+HADOOP_VERSION="2.3"
+FLOATING_IP_POOL="net04_ext"
+AUTO_SECURITY_GROUP=true
+CLUSTER_FLAVOR_ID="4"
+KEYPAIR_NAME="tmp_keypair"
+SECURITY_GROUPS=d
+NETWORK_NAME="net04"
+NETWORK_ID=`neutron net-list | grep net04[^_] | awk '{print$2}'`
+WORKING_DIR="/tmp/sahara_image"
+IMAGE_NAME="My_image_name"
+#######################
+# Copy files to temp.
+#######################
+cp -r *.json $WORKING_DIR/
+
+#############################################
+# Setup Master & Worker Node Group Template
+#############################################
+NETWORK_ID=`neutron net-list | grep net04[^_] | awk '{print$2}'`
+FLOATING_IP_POOL_ID=`openstack network list |grep $FLOATING_IP_POOL |awk '{print $2}'`
+for i in /tmp/sahara_image/*.json; do
+    sed -i "s/{CLUSTER_NAME}/$CLUSTER_NAME/" $i
+    sed -i "s/{PLUGIN_NAME}/$PLUGIN_NAME/" $i
+    sed -i "s/{HADOOP_VERSION}/$HADOOP_VERSION/" $i
+    sed -i "s/{FLOATING_IP_POOL_ID}/$FLOATING_IP_POOL_ID/" $i
+    sed -i "s/{AUTO_SECURITY_GROUP}/$AUTO_SECURITY_GROUP/" $i
+    sed -i "s/{CLUSTER_FLAVOR_ID}/$CLUSTER_FLAVOR_ID/" $i
+    sed -i "s/{EXTERNAL_NETWORK}/$EXTERNAL_NETWORK/" $i
+    sed -i "s/{KEY_PAIR_NAME}/$KEY_PAIR_NAME/" $i
+    sed -i "/^.*{SECURITY_GROUPS}.*$/d" $i
+    sed -i "s/{NETWORK_NAME}/$NETWORK_NAME/" $i
+    sed -i "s/{NETWORK_ID}/$NETWORK_ID/" $i
+done
+sahara node-group-template-create --json $WORKING_DIR/worker_node_template.json &> /dev/null
+echo "INFO: Worker Node Template Created"
+sahara node-group-template-create --json $WORKING_DIR/master_node_template.json &> /dev/null
+echo "INFO: Master Node Template Created"
+
+################################
+# Setup Cluster Group Template
+################################
+sleep 2
+WORKER_NODE_ID=`sahara node-group-template-list | grep $PLUGIN_NAME-worker | awk '{print$4}'`
+MASTER_NODE_ID=`sahara node-group-template-list | grep $PLUGIN_NAME-master | awk '{print$4}'`
+echo $WORKER_NODE_ID
+echo $MASTER_NODE_ID
+for i in $WORKING_DIR/*.json; do
+    sed -i "s/{WORKER_NODE_ID}/$WORKER_NODE_ID/" $i
+    sed -i "s/{MASTER_NODE_ID}/$MASTER_NODE_ID/" $i
+done
+sahara cluster-template-create --json $WORKING_DIR/cluster_template.json &> /dev/null
+echo "INFO: Cluster Group Template Created"
+
+##################
+# Deploy cluster
+##################
+sleep 2
+CLUSTER_TEMPLATE_ID=`sahara cluster-template-list |grep $PLUGIN_NAME-cluster | awk '{print $4}'`
+nova keypair-add $KEY_PAIR_NAME > $WORKING_DIR/KEY_PAIR_NAME.pem
+DEFAULT_IMAGE_ID=`nova image-list | grep $IMAGE_NAME | awk '{print$2}'`
+for i in $WORKING_DIR/*.json; do
+    sed -i "s/{CLUSTER_TEMPLATE_ID}/$CLUSTER_TEMPLATE_ID/" $i
+    sed -i "s/{KEYPAIR_ID}/$KEYPAIR_ID/" $i
+    sed -i "s/{DEFAULT_IMAGE_ID}/$DEFAULT_IMAGE_ID/" $i
+done
+
+sahara cluster-create --json $WORKING_DIR/my_cluster_create.json &> /dev/null
+echo "INFO: Cluster successfully deployed!"
